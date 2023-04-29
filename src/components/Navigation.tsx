5ca42a32b692e60daead2b306313afa399eceb55
Mitra Singh <mitra.coder@gmail.com>
2023-04-28 22:58:14 -0400

    added dropdown menu for user img

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Col, Container, Nav, Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Col, Container, Dropdown, Nav, NavDropdown, Navbar } from 'react-bootstrap'
 
 export const Navigation = () => {
   return (
-    <Navbar bg="light" variant="light">
-        <Container>
+    <Navbar bg="light" variant="light" className="px-5">
+        <Container fluid>
             <Col xs lg="2">
                 <Navbar.Brand href="/">
                     <img
@@ -18,6 +17,7 @@ export const Navigation = () => {
                 </Navbar.Brand>
                 <Navbar.Brand className="fw-bold" href="/">Apollo</Navbar.Brand>
             </Col> 
+            
             <Col md="auto">   
                 <Nav style={{fontSize: "11px"}} className="me-auto fw-bold mt-1">
                     <Nav.Link href="/" className="me-2">Home</Nav.Link>
@@ -25,11 +25,11 @@ export const Navigation = () => {
                     <Nav.Link href="/shoutboard" className="ms-2">Shout Board</Nav.Link>
                 </Nav>
             </Col>
-            <Col xs lg="2" className="d-flex justify-content-end">
-                
+            
+            <Col xs lg="2" className="d-flex justify-content-end">  
                 <Nav>
-                <Nav.Link style={{fontSize: "10px"}} className="fw-bold mt-1 " href="#pricing">Hello, Mitch!</Nav.Link>
-                    <Navbar.Brand href="/profile">
+                <Nav.Link style={{fontSize: "8px"}} className="fw-bold ">Hello, <br></br>Mitch!</Nav.Link>
+                    <Navbar.Brand>
                         <img
                         src="public/default-profile.png"
                         width="30"
@@ -38,8 +38,11 @@ export const Navigation = () => {
                         alt="user image"
                         />
                     </Navbar.Brand>
+                    <NavDropdown drop="down-centered" title="" menuVariant="light">
+                            <NavDropdown.Item style={{fontSize: "9px"}} href="">Edit Profile</NavDropdown.Item>
+                            <NavDropdown.Item style={{fontSize: "9px"}} href="">Logout</NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
-            
             </Col>
         </Container>
     </Navbar>
